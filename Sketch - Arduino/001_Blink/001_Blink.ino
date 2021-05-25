920574ea8f11001afde5d52c0b1af49ec640c602
ScuratovaAnna <skuratowaanna@yandex.ru>
2021-05-25 17:00:47 +0300

    create repository

--- Sketch - Arduino/001_Blink/001_Blink.ino
+++ Sketch - Arduino/001_Blink/001_Blink.ino
@@ -1,12 +1,17 @@
-
+//25.05.2021
+/* Библиотека SoftwareSerial позволяет реализовать последовательный интерфейс на любых цифровых выводах Ардуино с помощью
+ * программных средств, дублирующих функциональность UART (отсюда и название "SoftwareSerial"). Библиотека позволяет программно
+ * создавать несколько последовательных портов, работающих на скорости до 115200 бод
+ */
 #include <nvtSoftwareSerial.h>// подключаем библиотеку SoftwareSerial
-SoftwareSerial mySerial(10, 11); // назначим выводы RX, TX (PA.13 PA.12)
+SoftwareSerial mySerial(10, 11); // назначим выводы UART5_TXD, UART5_RXD (PA.13 PA.12)
+
 
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
   //pinMode(LED_BUILTIN, OUTPUT);
   GPIO_SetMode(PB, BIT13, GPIO_PMD_OUTPUT);
-  Serial.begin(9600);//Задает скорость передачи данных
+  Serial.begin(9600);//инициализируем и задает скорость передачи данных UART0_TXD, UART0_TXD
   //Задает скорость передачи данных для последовательного порта, созданного библиотекой
   mySerial.begin(115200);
   mySerial.println("Hello, world?");
