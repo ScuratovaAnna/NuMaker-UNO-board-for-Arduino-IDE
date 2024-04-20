9ec71daeeafddd5ee9664047b97d0a18fb529277
000 <pivnev_tosno@mail.ru>
2024-04-20 17:31:28 +0300

    Timer

--- Sketch - Arduino/001_Blink/005_Blink_Timer.ino
+++ Sketch - Arduino/001_Blink/005_Blink_Timer.ino
@@ -38,3 +38,37 @@ void SYS_Init(void){
   CLK_SetModuleClock(TMR0_MODULE, CLK_CLKSEL1_TMR0_S_HCLK, 0);
   SYS_LockReg();
 }
+/****************** Example 2 ***************************/
+volatile uint16_t count;
+void setup() {
+  GPIO_SetMode(PB, BIT13, GPIO_PMD_OUTPUT);
+}
+
+void loop() {
+  PB13=!PB13;
+  delay_ms(1);
+   /*
+  *   --    --    --
+  *  |  |  |  |  |  |
+  *  |  |__|  |__|  |__ period 2s Freq 500mHz
+  */
+  }
+/* Прерывание срабатывает один раз в секунду */
+void timer_ISR(uint8_t num)
+{
+     if (count)
+        count--;
+}
+
+void delay_ms(uint16_t ms)
+{
+  uint32_t clock = 12000000; /* 12Mhz */
+  Timer1.open(PERIODIC, clock);
+  Timer1.setPrescaleFactor(128);//Установим предделитель 128
+  Timer1.setCompare(93750);//12000000/128=93750
+  Timer1.attachInterrupt(timer_ISR);
+  Timer1.start();
+  count = ms;
+  while(count);
+}
+/********************************************************/
